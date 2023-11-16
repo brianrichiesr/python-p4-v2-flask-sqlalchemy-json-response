5c1c6b83759e56d213fe648db0d162e0faa582db
Brian Richie Sr <brianrichiesr@gmail.com>
2023-11-15 22:19:18 -0800

    complete lab

--- server/app.py
+++ server/app.py
@@ -16,17 +16,43 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {"message": "Welcome to the pet directory!"}
+    return make_response(body, 200)
 
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = db.session.get(Pet, id)
+    if pet:
+        body = {
+            "id": pet.id,
+            "name": pet.name,
+            "species": pet.species
+        }
+        status = 200
+    else:
+        body = {
+            "body": {"message": f"Pet {id} not found."}
+        }
+        status = 404
+    return make_response(body, status)
 
-@app.route('/demo_json')
-def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+@app.route("/species/<string:species>")
+def pet_by_species(species):
+    pet_list = []
+    pets = Pet.query.filter_by(species = species.title()).all()
+    for pet in pets:
+        pet_dict = {
+            "id": pet.id,
+            "name": pet.name,
+        }
+        pet_list.append(pet_dict)
 
+    body = {
+        "count": len(pet_list),
+        "pets": pet_list
+    }
 
+    return make_response(body, 200)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
